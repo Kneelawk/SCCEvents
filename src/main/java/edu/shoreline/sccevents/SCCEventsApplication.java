f2e5d4726b3a400d40ae8d417765fe81256657a8
Kneelawk <jedidiah.pommert@gmail.com>
2020-02-26 16:56:35 -0800

    Add basic web server system

---
+++ src/main/java/edu/shoreline/sccevents/SCCEventsApplication.java
@@ -0,0 +1,11 @@
+package edu.shoreline.sccevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCCEventsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SCCEventsApplication.class, args);
+    }
+}
